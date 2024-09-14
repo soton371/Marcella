66034bde156aad030edc75c7dac8f3f807c36234
Soton Ahmed <soton371@gmail.com>
2024-09-14 12:24:51 +0600

    test

--- main.py
+++ main.py
@@ -1,9 +1,58 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+# from . import crud, models, schemas
+from models import *
+from schemas import *
+from crud import *
+
+
+from database import SessionLocal, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get('/')
-def hello():
-    return {'message': 'Hello world!'}
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
+@app.post("/users/", response_model=UserP)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return create_userC(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[UserP])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=UserP)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=ItemP)
+def create_item_for_user(
+    user_id: int, item: ItemCreate, db: Session = Depends(get_db)
+):
+    return create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[ItemP])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = get_items(db, skip=skip, limit=limit)
+    return items
---
+++ schemas.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class ItemP(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserP(UserBase):
+    id: int
+    is_active: bool
+    items: list[ItemP] = []
+
+    class Config:
+        orm_mode = True
